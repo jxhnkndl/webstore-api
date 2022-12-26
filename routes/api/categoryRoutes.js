fcafbab123177bc4e068cffa356b36f36cbd6042
jxhnkndl <jkroyston@gmail.com>
2022-12-26 17:54:16 -0500

    create GET, POST, PUT, and DELETE routes for Category model

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -1,19 +1,84 @@
 const router = require('express').Router();
-const {Category} = require('../../models');
+const { Category } = require('../../models');
 
 // GET all categories -> /api/categories
-router.get('/', (req, res) => {});
+router.get('/', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.findAll();
+
+    res.json(dbCategoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // GET category by id -> /api/categories/:id
-router.get('/:id', (req, res) => {});
+router.get('/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.findOne({
+      where: { id: req.params.id }
+    });
+
+    res.json(dbCategoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // POST new category -> /api/categories
-router.post('/', (req, res) => {});
+router.post('/', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+
+    res.json(dbCategoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // UPDATE category by id -> /api/categories/:id
-router.put('/:id', (req, res) => {});
+router.put('/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!dbCategoryData) {
+      res.status(404).json({ msg: 'Category not found' });
+      return;
+    }
+
+    res.json(dbCategoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // DELETE category by id -> /api/categories/:id
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+
+    if (!dbCategoryData) {
+      res.status(404).json({ msg: 'Category not found' });
+      return;
+    }
+
+    res.json(dbCategoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
