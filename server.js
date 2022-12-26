95cf5c27b0e0c26bcca45240f05b6b19d769a844
jxhnkndl <jkroyston@gmail.com>
2022-12-26 17:55:10 -0500

    add port argument to listen method

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +9,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// configure routes
+app.use(routes);
+
 // connect to db and start server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(() => console.log(`Listening on PORT ${PORT}`));
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
