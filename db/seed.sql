f6a83eabe398305971f26650d36e2f4f66540543
jxhnkndl <jkroyston@gmail.com>
2022-12-27 17:42:52 -0500

    add Product model data to seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,12 @@
+DELETE FROM product;
+DELETE FROM category;
+DELETE FROM tag;
+
 INSERT INTO category (category_name)
 VALUES
   ('apparel'),
   ('records'),
-  ('tickets');
+  ('stickers');
 
 INSERT INTO tag (tag_name)
 VALUES
@@ -11,3 +15,13 @@ VALUES
   ('hats'),
   ('limited'),
   ('sale');
+
+INSERT INTO product (product_name, price, stock, category_id)
+VALUES
+  ('black tee', 25.00, 10, 1),
+  ('red tee', 25.00, 10, 1),
+  ('green beanie', 20.00, 20, 1),
+  ('battle hymn', 15.00, 30, 2),
+  ('fiction fever', 10.00, 10, 2),
+  ('hey summer rectangle', 5.00, 10, 3),
+  ('logo circle', 5.00, 10, 3);
