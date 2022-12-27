6b6588aefe2022aef236b87bb5bb4260a55a0ea2
jxhnkndl <jkroyston@gmail.com>
2022-12-26 19:59:41 -0500

    create GET, POST, PUT, and DELETE routes for Tag model

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const categoryRoutes = require('./categoryRoutes');
+const tagRoutes = require('./tagRoutes');
 
 // prefix and batch api routes
 router.use('/categories', categoryRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
